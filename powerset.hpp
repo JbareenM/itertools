1ed7495b26dc6644faac26b148c8314e20d45aa0
JbareenM <44744884+JbareenM@users.noreply.github.com>
2019-05-26 21:48:09 +0300

    Add files via upload

--- powerset.hpp
+++ powerset.hpp
@@ -5,42 +5,72 @@
 //  Created by Jeme Jbareen on 5/14/19.
 //  Copyright © 2019 Jeme Jbareen. All rights reserved.
 //
+//https://stackoverflow.com/questions/24365954/how-to-generate-a-power-set-of-a-given-set
 
 #ifndef powerset_hpp
 #define powerset_hpp
-#include<iostream>
-#include<sstream>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <iostream>
 
 namespace itertools{
-    template <typename T>
+    template<typename T>
     class powerset{
     private:
         T _set;
     public:
-        powerset(const T s1):_set(s1){}
-        //iterator_class________________________________________________________
-        class _iterator{
+        powerset(const T set):_set(set){}
+         //iterator_class________________________________________________________
+        class const_iterator {
         private:
-            typename T::_iterator _cur;
+            const T& _cur;
+            uint _Total;
         public:
-            _iterator(const typename T::_iterator cur):_cur(cur){}
-            _iterator &operator++(){
-                _cur++;
+            const_iterator(const T& cur, int index):_cur(cur), _Total(index){}
+            const_iterator& operator++() {
+                ++_Total;
                 return *this;
             }
-            auto &operator*() const{
-                std::ostringstream ostr;
-                ostr<<"";
-                return ostr.str();
+            auto operator*() const {
+                vector<typename remove_const<typename remove_reference<decltype(*(_set.begin()))>::type>::type> ans;
+                int i=1;
+                for (auto j : _cur){
+                    if (i & _Total)
+                        ans.push_back(j);
+                    i=i<<1;
+                }
+                return ans;
             }
-            bool operator!=(const _iterator &rhs) const{
-                return _cur != rhs._cur;
+            const const_iterator operator++(int) {
+                const_iterator tmp= *this;
+                ++_Total;
+                return tmp;
             }
+            bool operator==(const const_iterator& rhs) const {_Total == rhs._Total;}
+            bool operator!=(const const_iterator& rhs) const {return !(_Total == rhs._Total);}
         };//iterator_class________________________________________________________
         //_____________________________________________________
-        auto begin() const{return (_set.begin());}
-        auto end() const{return (_set.end());}
+        auto begin() const {return const_iterator(_set, 0);}
+        auto end() const {
+            int size = 0;
+            for (auto i : _set)
+                size += 1;
+            return const_iterator(_set, int(pow(2,size)));
+        }
     };
+    template <typename IT>
+    std::ostream &operator<<(std::ostream &out, const std::vector<IT> &vec){
+        out << "{";
+        auto i = vec.begin();
+        while (i != vec.end()){
+            out<<*i;
+            i++;
+            if (i != vec.end())
+                out<<",";
+        }
+        out << "}";
+        return out;
+    }
 }
-
 #endif /* powerset_hpp */
